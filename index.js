ec6088685fd962aa66cf5bd0c0f238a745fb510d
imthesk01 <imthesk01@users.noreply.github.com>
2020-12-10 19:40:06 +0530

    update home and JS with animation fn

--- index.js
+++ index.js
@@ -17,3 +17,22 @@ function doRedirect(href) {
  window.location = href;
 
 };
+
+function hover1(){
+  $("#item1-des").slideToggle();
+};
+function hover2(){
+  $("#item2-des").slideToggle();
+};
+function animate_big(){
+	$("#buy").animate({
+    width:"200px"
+  //  $("div").width("50%");
+})
+};
+function animate_small(){
+	$("#buy").animate({
+    width:"100px"
+  //  $("div").width("50%");
+})
+};
